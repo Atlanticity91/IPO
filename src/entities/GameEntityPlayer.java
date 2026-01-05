eb405a124b54b50390d1928f4963e0639c5b1e71
Alves Quentin <atlanticity91@gmail.com>
2026-01-05 23:55:42 +0100

    FIX BOF

--- src/entities/GameEntityPlayer.java
+++ src/entities/GameEntityPlayer.java
@@ -171,7 +171,10 @@ public class GameEntityPlayer extends GameEntity {
         if (new_tile instanceof GameTileInteractable interactable ) {
             final Vector2 offset = getDimensions( ).sub( old_location );
 
-            interactable.onEnter(state_manager, tilemap, (GameTileInteractable) current_tile, this, offset );
+            if ( current_tile instanceof GameTileInteractable i )
+                interactable.onEnter(state_manager, tilemap, i, this, offset );
+            else
+                interactable.onEnter(state_manager, tilemap, null, this, offset );
         }
     }
 
